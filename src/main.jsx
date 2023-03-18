f034074983580861a50712ff74727da8ce171563
Bhakti <bkhedkar79@gmail.com>
2023-03-18 12:52:54 +0530

    1st commit

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+
+//For Output :- use :- 
+//1]npm run dev
+//2] npm run build (it creates dist folder)
